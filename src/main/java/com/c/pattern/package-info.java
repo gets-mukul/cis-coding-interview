0bc5cdb2482d46e5192e98e900614e8fd54cf756
gets-mukul <mukulpraj9@gmail.com>
2018-11-04 14:14:08 +0530

    pattern02

--- src/main/java/com/c/pattern/package-info.java
+++ src/main/java/com/c/pattern/package-info.java
@@ -6,3 +6,289 @@
  *
  */
 package com.c.pattern;
+
+/*
+ * Q.1
+1
+1 2 1
+1 2 3 2 1
+1 2 3 4 3 2 1
+1 2 3 4 5 4 3 2 1
+1 2 3 4 5 6 5 4 3 2 1
+1 2 3 4 5 6 7 6 5 4 3 2 1
+
+
+Q.2
+1 2 3 4 5 6 7
+1 2 3 4 5 6
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4 5 6
+1 2 3 4 5 6 7
+
+
+Q. 3
+
+1111111
+1111122
+1111333
+1114444
+1155555
+1666666
+7777777
+
+
+
+Q. 4 
+1 2 3 4 5 6 7
+ 2 3 4 5 6 7
+  3 4 5 6 7
+   4 5 6 7
+    5 6 7
+     6 7
+      7
+     6 7
+    5 6 7
+   4 5 6 7
+  3 4 5 6 7
+ 2 3 4 5 6 7
+1 2 3 4 5 6 7
+
+
+
+
+
+Q. 5 
+
+1
+2 6
+3 7 10
+4 8 11 13
+5 9 12 14 15
+
+Q.6 W.A.P. for following paramid.
+
+1 
+1 0
+1 0 1
+1 0 1 0
+1 0 1 0 1
+1 0 1 0 1 0
+
+Q.7 W.A.P. for following paramid.
+
+1
+2 1 2
+3 2 1 2 3
+4 3 2 1 2 3 4
+5 4 3 2 1 2 3 4 5
+6 5 4 3 2 1 2 3 4 5 6
+
+Q.8. W.A.P. for following paramid.
+
+1
+2 1
+3 2 1
+4 3 2 1
+5 4 3 2 1
+6 5 4 3 2 1
+
+Q9. W.A.P. for following paramid.
+
+6 5 4 3 2 1
+5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1
+1
+
+Q.10. W.A.P. for following paramid.
+
+6 5 4 3 2 1 2 3 4 5 6
+5 4 3 2 1 2 3 4 5
+4 3 2 1 2 3 4
+3 2 1 2 3
+2 1 2
+1
+
+
+
+
+
+
+Q.11 W.A.P. for following paramid.
+1
+2 3 2
+3 4 5 4 3
+4 5 6 7 6 5 4
+5 6 7 8 9 8 7 6 5
+
+Q.12 W.A.P. for following paramid.
+1 2 3 4 5 5 4 3 2 1
+1 2 3 4 4 3 2 1
+1 2 3 3 2 1
+1 2 2 1
+1 1
+
+
+Q.13. W.A.P. for following paramid.
+
+A 
+B C 
+D E F 
+G H I J 
+K L M N O
+
+Q.14. W.A.P. for following paramid.
+
+2
+2 4
+2 4 6
+2 4 6 8
+2 4 6 8 10
+2 4 6 8 10 12
+
+
+Q.15. W.A.P. for following paramid.
+
+1
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+0 1 0 1 0 1
+
+Q.16  Write a program to make a pattern mentioned below:
+_ _ _ _ 1
+3 3 _ _ _ _ _3 3
+5 5 5 _ _ _ 5 5 5
+7 7 7 7 7 7 7 7 7
+
+Q.17 Write a program to make a pattern mentioned below:
+1 1 1 1 1
+1 2 2 2 1
+1 2 3 2 1
+1 2 2 2 1
+1 1 1 1 1
+
+Q.18 Write a program to make a pattern mentioned below:
+
+0
+1 _ 0 _ 1
+0 _ _1 _ _0
+1 _ 0 _ 1
+0
+
+Q.19. Write a program to make a pattern mentioned below:
+_ _ _ *
+_ _ * A *
+_ * A * A *
+* A * A * A *
+
+Q.20. Write a program to make a pattern mentioned below:
+_ _ _ 1
+_ _ 2 3 2
+_ 3 4 5 4 3
+4 5 6 7 6 5 4
+
+Q.21. Write a program to find Transpose of the given Matrix:
+1 2 9
+0 4 7
+7 3 2
+(output expected)
+1 0 7
+2 4 3
+9 7 2
+
+Q.22. Write a program to print half pyramid using continuous characters starting
+from 'A'.
+A
+C E
+G I K
+M O Q S
+B D F
+H J
+L
+
+Q.23  Write a program to make a pattern mentioned below:
+1
+3 2
+6 5 4
+7 8 9 10
+
+Q.24 Write a program to make a pattern mentioned below:
+* _ _ _ _ *
+* _ _ _ *
+* _ _ *
+* _ *
+*
+* _ *
+* _ _ *
+* _ _ _ *
+* _ _ _ _ *
+
+
+Q.25. Write a program to make a Pascal Triangle pattern mentioned below:
+------1
+-----1 1
+----1 2 1
+---1 3 3 1
+--1 4 6 4 1
+1 5 10 10 5 1
+
+Q.26. Write a program to make a pattern mentioned below:
+- - - - - 5
+- - - 4 0 0 0 6
+- - 3 0 0 0 0 0 7
+- 2 0 0 0 0 0 0 0 8
+1 0 0 0 0 0 0 0 0 0 9
+
+Q.27 Write a program to make zig zag pattern mentioned below:
+- - - - 5 - - - - - - - 13
+- - - 4 - 6 - - - - - 12
+- - 3 - - - 7 - - - 11
+- 2 - - - - - 8 - 10
+1 - - - - - - - 9
+
+Q.28 Write a program to make a pattern mentioned below:
+- - - 1
+- - 3 - 5
+- 7 - 9 - 11
+13 - 15 - 17 - 19
+
+Q.29. Write a program to make a pattern mentioned below:
+
+**
+*
+* _ *
+* _ _ *
+* _ _ _ *
+* _ _ _ _*
+* _ _ _ _ _*
+* _ _ _ _ _ _*
+* * * * * * * * *
+
+Q.30. Write a program to make a pattern mentioned below:
+0-------0
+0-----0
+0---0
+0-0
+0---0
+0-----0
+0-------0
+
+
+
+
+
+
+
+
+ * */
