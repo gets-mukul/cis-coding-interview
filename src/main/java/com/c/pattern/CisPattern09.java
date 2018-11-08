a9f4dcb626e7fe8b2b1464ebdcc66c6b2abea235
gets-mukul <mukulpraj9@gmail.com>
2018-11-08 23:12:41 +0530

    pattern09

---
+++ src/main/java/com/c/pattern/CisPattern09.java
@@ -0,0 +1,37 @@
+/**
+6 5 4 3 2 1
+5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1
+1
+ */
+package com.c.pattern;
+
+import java.util.Scanner;
+
+/**
+ * @author mukul
+ *
+ */
+public class CisPattern09 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the value of n:");
+		int n = sc.nextInt();
+		
+		for(int i=n; i>=1; i--) {
+			for(int j=i; j>=1; j--) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
