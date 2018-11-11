01063f5eb9f827502d4c11c2bd4193f2775eb401
gets-mukul <mukulpraj9@gmail.com>
2018-11-11 18:19:10 +0530

    pattern 04

--- src/main/java/com/c/pattern/CisPattern04.java
+++ src/main/java/com/c/pattern/CisPattern04.java
@@ -41,13 +41,12 @@ public class CisPattern04 {
 			}
 			System.out.println();
 		}
-
-		for (int i = 2; i <= n; i++) {
-			for (int j = 1; j <= (n - i); j--) {
-				System.out.print(" ");
+		for(int i=(n-1); i>=1; i--) {
+			for(int j=(i-1); j>=1; j--) {
+				System.out.print(" "	);
 			}
-			for (int j = (n-i); j < n; j++) {
-				System.out.print((j+1) + " ");
+			for(int j=i; j<=n; j++) {
+				System.out.print(j+" ");
 			}
 			System.out.println();
 		}
