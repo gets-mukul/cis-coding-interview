1c6f543d293cca4cc07c1e664388eaee223d7374
gets-mukul <mukulpraj9@gmail.com>
2018-11-26 20:52:44 +0530

    find ketter frequency

--- src/main/java/com/c/pattern/LetterFrequency.java
+++ src/main/java/com/c/pattern/LetterFrequency.java
@@ -3,10 +3,8 @@
  */
 package com.c.pattern;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 /**
  * @author mukul
@@ -19,27 +17,14 @@ public class LetterFrequency {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();						//input of string
-		str = str.toLowerCase();						// convert string to lowercase
+		int[] arr = { 5,3,7,6,8,1,9,4,0,2};
 		
-		HashMap<Character, Integer> frequency_hash = new HashMap<Character, Integer>();
+		Arrays.sort(arr, 0, 4);
+
 		
-		for(int i=0; i<str.length(); i++) {
-			if(str.charAt(i)!=' ') {
-				if(frequency_hash.containsKey(str.charAt(i))) {									//check for current character exist, if exist add value by one
-					frequency_hash.put(str.charAt(i), frequency_hash.get(str.charAt(i))+1);
-				}
-				else {																			// else put the key and initialise value by 1
-					frequency_hash.put(str.charAt(i), 1);
-				}
-			}
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]);
 		}
-		
-		for (Map.Entry entry : frequency_hash.entrySet()) { 
-            System.out.println(entry.getKey() + " " + entry.getValue()); 
-        } 
-		
 
 	}
 
